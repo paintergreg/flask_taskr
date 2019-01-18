ca34223e2923d2f20c82e413b228f90ac6dee20e
Greg Painter <greg.painter@gmail.com>
2019-01-18 07:43:11 -0700

    Changed database access to SQLAlchemy.  Implemented views with WT-Forms

--- project/_config.py
+++ project/_config.py
@@ -19,8 +19,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DATABASE = "flasktaskr.db"
 USERNAME = "admin"
 PASSWORD = "admin"
-WTF_CSRF_ENABLED = True
+CSRF_ENABLED = True
 SECRET_KEY = "\xa5\x88\x8a\xa4\x04]\x9c\xfc-%\xb7\x90\x99\x96V1y\x98G\x93P\x95(\xb7"
 
 # define the full path for the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + DATABASE_PATH
+SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ project/models.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+#
+################
+#
+# project/models.py
+#
+################
+#
+
+"""
+    SQLAlchemy ORM Model
+"""
+
+from views import db
+
+
+class Task(db.Model):
+
+    __tablename__ = "tasks"
+
+    task_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    due_date = db.Column(db.Date, nullable=False)
+    priority = db.Column(db.Integer, nullable=False)
+    status = db.Column(db.Integer)
+
+    def __init__(self, name, due_date, priority, status):
+        self.name = name
+        self.due_date = due_date
+        self.priority = priority
+        self.status = status
+
+    def __repr__(self):
+        return f"<name {self.name}>"
